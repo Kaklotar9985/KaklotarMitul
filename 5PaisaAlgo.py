f205f33f687e2f04a16156018ea6bbe2e0c23075
Kaklotar9985 <kaklotar.mitul@gmail.com>
2025-04-24 13:00:11 +0530

    Update 5PaisaAlgo.py

--- 5PaisaAlgo.py
+++ 5PaisaAlgo.py
@@ -1,8 +1,11 @@
+# login_to_5paisa  login_to_5paisa   login_to_5paisa   login_to_5paisa   login_to_5paisa   login_to_5paisa   login_to_5paisa   login_to_5paisa   login_to_5paisa   login_to_5paisa   
+
+#!pip install py5paisa
+#!pip install pyotp
 from py5paisa import FivePaisaClient
 from py5paisa.order import OrderType, Exchange
 import pyotp
 
-# login_to_5paisa  login_to_5paisa   login_to_5paisa   login_to_5paisa   login_to_5paisa   login_to_5paisa   login_to_5paisa   login_to_5paisa   login_to_5paisa   login_to_5paisa   
 def login_to_5paisa(cred):
     try:
         totp = pyotp.TOTP(cred["token"]).now()
@@ -19,3 +22,26 @@ def login_to_5paisa(cred):
         print("❌ 5paisa Login Failed:", str(e))
         return None
 #____________________________________________________________________________________________________________________________________________________________________________________
+
+#  login_to_Anjal   login_to_Anjal    login_to_Anjal    login_to_Anjal    login_to_Anjal    login_to_Anjal    login_to_Anjal    login_to_Anjal   
+# !pip install smartapi-python
+# !pip install logzero
+from SmartApi import SmartConnect
+from SmartApi.smartWebSocketV2 import SmartWebSocketV2
+import pyotp
+
+def login_to_Anjal(LoginData):
+    try:
+        smartApi = SmartConnect(api_key=LoginData["API_KEY"])
+        session_data = smartApi.generateSession(LoginData["USERNAME"], LoginData["PWD"], pyotp.TOTP(LoginData["TOKEN"]).now())
+        AUTH_TOKEN = session_data['data']['jwtToken']
+        refreshToken = session_data['data']['refreshToken']
+        FEED_TOKEN = smartApi.getfeedToken()
+        profile = smartApi.getProfile(refreshToken)
+        sws = SmartWebSocketV2(AUTH_TOKEN, LoginData["API_KEY"], LoginData["USERNAME"], FEED_TOKEN)
+        print("Login Successful : ", profile['data']['name'])
+        return smartApi, AUTH_TOKEN, refreshToken, FEED_TOKEN, sws
+        # return { 'smartApi': smartApi, 'auth_token': AUTH_TOKEN, 'refresh_token': refreshToken, 'feed_token': FEED_TOKEN, 'websocket': sws }
+    except Exception as e:
+        print("❌ Anjal Login Failed:", str(e))
+#______________________________________________________________________________________________________________________________________________
