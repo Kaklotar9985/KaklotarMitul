7b43c78b32e8cfbcc302423afd933549585a8337
Kaklotar9985 <kaklotar.mitul@gmail.com>
2025-04-24 10:54:19 +0530

    5PaisaAlgo.py

---
+++ 5PaisaAlgo.py
@@ -0,0 +1,18 @@
+from py5paisa import FivePaisaClient
+from py5paisa.order import OrderType, Exchange
+import pyotp
+
+# login_to_5paisa  login_to_5paisa   login_to_5paisa   login_to_5paisa   login_to_5paisa   login_to_5paisa   login_to_5paisa   login_to_5paisa   login_to_5paisa   login_to_5paisa   
+def login_to_5paisa(cred):
+    try:
+        totp = pyotp.TOTP(cred["token"]).now()
+        client = FivePaisaClient(cred=cred)
+        client.get_totp_session(cred["ClientCode"], totp, cred["pin"])
+        access_token = client.get_access_token()
+        client.set_access_token(access_token, cred["ClientCode"])
+        print("✅ Login Successful to 5Paisa for Client:", cred["ClientCode"])
+        return client
+    except Exception as e:
+        print("❌ 5paisa Login Failed:", str(e))
+        return None
+#____________________________________________________________________________________________________________________________________________________________________________________
