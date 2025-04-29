25ba11646ac748b440aa4de1ac7754ee823055af
Kaklotar9985 <kaklotar.mitul@gmail.com>
2025-04-29 10:32:53 +0530

    Update 5PaisaAlgo.py

--- 5PaisaAlgo.py
+++ 5PaisaAlgo.py
@@ -47,85 +47,85 @@ def login_to_Anjal(LoginData):
 #______________________________________________________________________________________________________________________________________________
 
 # fetch_Date_Time # fetch_Date_Time # fetch_Date_Time # fetch_Date_Time # fetch_Date_Time # fetch_Date_Time # fetch_Date_Time # fetch_Date_Time # fetch_Date_Time
-from datetime import datetime, timedelta
-import threading
-import time
+
+from datetime import datetime, timedelta, time
 import pytz
 
-def fetch_Date_Time():
-    global live_time , live_date , Candle_Date , First_Candle_Time , Previous_Candal_Time
-    max_retries = 3
-    retries = 0
-
-    while retries < max_retries:  # री-ट्राई चक्र  datetime
-        try :
-            ist = pytz.timezone('Asia/Kolkata')
-            now = datetime.now(ist)
-            live_time = now.strftime("%H:%M:%S")  # समय (घंटा:मिनट:सेकंड)
-            live_date = now.strftime("%d-%m-%Y")  # तारीख (दिन-महीना-वर्ष)
-
-            First_Candle_Time = "09:15"
-
-            # यदि लाइव डेट शनिवार या रविवार है, तो शुक्रवार की तारीख निकालें
-            if now.weekday() == 5:  # शनिवार
-                Candle_Date = (now - timedelta(days=1)).strftime("%Y-%m-%d")
-            elif now.weekday() == 6:  # रविवार
-                Candle_Date = (now - timedelta(days=2)).strftime("%Y-%m-%d")
-            elif live_time < "09:15:01":
-                Candle_Date = (now - timedelta(days=1)).strftime("%Y-%m-%d")
-            else:
-                Candle_Date = now.strftime("%Y-%m-%d")  # अन्य दिनों में लाइव डेट का उपयोग करें
-
-            # मिनट को राउंड करके पिछले 5 मिनट की कैंडल का समय निकालें
-            minute = now.minute
-            rounded_minute = (minute // 5) * 5 - 5
-
-            if rounded_minute < 0:
-                rounded_minute = 55
-                now = now - timedelta(hours=1)
-
-            Time_rounded = now.replace(minute=rounded_minute, second=0, microsecond=0).strftime("%H:%M")
-
-            if "15:30" >= Time_rounded >= "09:15":
-                Previous_Candal_Time = Time_rounded
-            else:
-                Previous_Candal_Time = "15:25"
-
-            # print("fetch_Date_Time successfully")
-            # सफल होने पर लूप से बाहर निकलें
-            break
-
-        except Exception as e:   # त्रुटि संदेश और री-ट्राई लॉजिक
-            retries += 1
-            Error = f"fetch_Date_Time function Error :"
-            print( Error, e )
-            time.sleep(1)  # री-ट्राई से पहले थोड़ी प्रतीक्षा करें
-
-    # यदि अधिकतम री-ट्राई के बाद भी डेटा न मिले
-    if retries == max_retries:
-        Error = f"fetch_Date_Time function Error Max Re-try : {max_retries}"
-        print(Error)
-
-# # उदाहरण के लिए
-fetch_Date_Time()
-# threading.Thread(target=fetch_Date_Time).start()
-# print("Live Date:", live_date)
-# print("Live Time:", live_time)
-# print("Candle Date:", Candle_Date)
-# print("First Candle Time:", First_Candle_Time)
-# print("Previous Candle Time:", Previous_Candal_Time)
-#_________________________________________________________________________________________________________________________________________________________________
-
-# Live_Time_Run_Function  # Live_Time_Run_Function  # Live_Time_Run_Function  # Live_Time_Run_Function  # Live_Time_Run_Function  # Live_Time_Run_Function
-def Run_fetch_Date_Time():
+IST = pytz.timezone('Asia/Kolkata')
+FORMATS = {"live_time": "%H:%M:%S", "live_date": "%d-%m-%Y","live_datetime": "%Y-%m-%d %H:%M:%S"}
+def get_live_datetime(Times = None):
     try:
-        while True:
-             fetch_Date_Time()
+       current = datetime.now(IST)
+       return current.strftime(FORMATS[Times.lower()]) if Times else current
+    except KeyError:
+        return datetime.now(IST).strftime(FORMATS["live_datetime"])
     except Exception as e:
-        Error =  f"Run_fetch_Date_Time function Error: {e}"
-        print(Error)
-threading.Thread(target=Run_fetch_Date_Time).start()
-#________________________________________________________________________________________________________________________________________________________________
+        print(f"get_live_time function Error : {e}")
+        return None
+
+# Example usage
+# print(get_live_datetime())  #  # live_time, live_date, live_datetime
+
+def get_last_trading_date(fmt = None):
+    try:
+        current_dt = get_live_datetime()
+        if current_dt.weekday() == 5:  # शनिवार
+            adjusted = current_dt - timedelta(days=1)
+        elif current_dt.weekday() == 6:  # रविवार
+            adjusted = current_dt - timedelta(days=2)
+        else:
+            if current_dt.time() < time(9,15,1):
+                adjusted = current_dt - timedelta(days=1)
+                if adjusted.weekday() == 5:  # नया दिन शनिवार
+                    adjusted -= timedelta(days=1)
+                elif adjusted.weekday() == 6:  # नया दिन रविवार
+                    adjusted -= timedelta(days=2)
+            else:
+                adjusted = current_dt
+        return adjusted.strftime(fmt) if fmt else adjusted
+    except Exception as e:
+        print(f"get_last_trading_day function Error : {e}")
+        return None
+
+# Example usage
+# print(get_last_trading_date())
+
+def get_candle_times(time_frame=5, Candle_no=-1, format = None):
+    try:
+      current = get_live_datetime()
+      last_day = get_last_trading_date()
+      market_start = last_day.replace(hour=9, minute=15, second=0, microsecond=0)
+      market_end = market_start.replace(hour=15, minute=30)
+      if Candle_no < 0:  # सभी negative candle numbers को हैंडल करें
+          steps_back = abs(Candle_no)
+          if market_start <= current <= market_end:
+              total_min = (current.hour * 60 + current.minute) - 555
+              prev_candle_min = (total_min // time_frame) * time_frame
+              base_candle = market_start + timedelta(minutes=prev_candle_min)
+          else:
+              base_candle = market_end - timedelta(minutes=time_frame)
+          desired_candle = base_candle - timedelta(minutes=(steps_back-1)*time_frame)
+          if desired_candle < market_start:
+              desired_candle = market_start
+          return desired_candle.strftime(format) if format else desired_candle
+      else:
+          delta = market_end - market_start
+          total_trading_minutes = delta.total_seconds() // 60
+          max_candles = ((total_trading_minutes - time_frame) // time_frame) + 1
+          Candle_no = max(1, min(Candle_no, max_candles))
+          start_time = market_start + timedelta(minutes=(Candle_no-1)*time_frame)
+          return start_time.strftime(format) if format else start_time
+    except Exception as e:
+        print(f"get_candle_times function Error : {e}")
+        return None
+
+# Example usage
+# time_Frem = 5
+# Candle_no = -1
+# format = "%H:%M"
+# previous = get_candle_times(time_Frem, Candle_no, format)
+# print(f"कैंडल: {previous}")
+#___________________________________________________________________________________________________________________________________________________________________________
 
 # fetch_Scrip_Data   fetch_Scrip_Data   fetch_Scrip_Data   fetch_Scrip_Data   fetch_Scrip_Data   fetch_Scrip_Data   fetch_Scrip_Data   fetch_Scrip_Data   fetch_Scrip_Data
 from datetime import datetime, timedelta
@@ -140,7 +140,7 @@ def fetch_All_Scrip_Data():
     except Exception as e:
         print(f"fetch_Scrip_Data Function Error: {e}")
         return None
-All_Scrip_Data = fetch_All_Scrip_Data()
+# All_Scrip_Data = fetch_Scrip_Data()
 #___________________________________________________________________________________________________________________________________________________________________________
 
 # fetch_Scrip_Data # fetch_Scrip_Data # fetch_Scrip_Data # fetch_Scrip_Data # fetch_Scrip_Data # fetch_Scrip_Data # fetch_Scrip_Data # fetch_Scrip_Data
@@ -153,7 +153,8 @@ def fetch_Scrip_Data(All_Scrip_Data):
         return Scrip_Data
     except Exception as e:
         print("fetch_Scrip_Data function Error : ", e )
-Scrip_Data = fetch_Scrip_Data(All_Scrip_Data)
+# Example usage
+# Scrip_Data = fetch_Scrip_Data(All_Scrip_Data)
 #________________________________________________________________________________________________________________________________________________________________________
 
 # fetch_ExpiryDate  # fetch_ExpiryDate  # fetch_ExpiryDate  # fetch_ExpiryDate  # fetch_ExpiryDate  # fetch_ExpiryDate  # fetch_ExpiryDate  # fetch_ExpiryDate
@@ -167,11 +168,12 @@ def fetch_ExpiryDate(Scrip_Data):
         return Curnent_Expiry , Next_Expiry ,Expiry_List
     except Exception as e:   # त्रुटि संदेश और री-ट्राई लॉजिक
         print( f"fetch_ExpiryDate function Error : ", e )
-Curnent_Expiry, Next_Expiry, Expiry_List  = fetch_ExpiryDate (Scrip_Data)
+# Example usage
+# Curnent_Expiry, Next_Expiry, Expiry_List  = fetch_ExpiryDate (Scrip_Data)
 #_________________________________________________________________________________________________________________________________________________________________
 
 # Symbol_SymbolToken  # Symbol_SymbolToken  # Symbol_SymbolToken  # Symbol_SymbolToken  # Symbol_SymbolToken  # Symbol_SymbolToken  # Symbol_SymbolToken  # Symbol_SymbolToken
-def Symbol_SymbolToken(Scrip_Data, Strike ,Option_Type, Expiry = Next_Expiry, Index = "NIFTY"):
+def Symbol_SymbolToken(Scrip_Data, Strike ,Option_Type, Expiry, Index = "NIFTY"):
     try:
         Token = f"{Index}{Expiry}{Strike}{Option_Type}"
         SymbolToken = Scrip_Data[Scrip_Data['symbol'].str.contains(Token, case=False, na=False)]['token'].iloc[0]
@@ -179,74 +181,67 @@ def Symbol_SymbolToken(Scrip_Data, Strike ,Option_Type, Expiry = Next_Expiry, In
         return Symbol , SymbolToken
     except Exception as e:
         print( f"Symbol_SymbolToken function Error : ", e )
-# # उदाहरण के लिए
+# Example usage
 # Symbol,SymbolToken = Symbol_SymbolToken( Scrip_Data, "24200", "CE", Expiry = Next_Expiry)
 # print(f"Symbol      : {Symbol}")
 # print(f"SymbolToken : {SymbolToken}")
 #______________________________________________________________________________________________________________________________________________________________________________
 
 # fetch_Candal_Data  # fetch_Candal_Data  # fetch_Candal_Data  # fetch_Candal_Data  # fetch_Candal_Data  # fetch_Candal_Data  # fetch_Candal_Data
-# Candal_Data = {}
-def fetch_Candal_Data(symboltoken, Symbol, StrikePrice, OP_tipe, smartApi):
-    global Candle_Date, First_Candle_Time, Previous_Candal_Time
-
+import pandas as pd
+from concurrent.futures import ThreadPoolExecutor
+def fetch_Candle_Data_Fast(symboltoken, Symbol, StrikePrice, OP_tipe, last_trading_date, First_Candle_Time, Previous_Candle_Time, smartApi):
     try:
-        if "09:20:00" <= live_time <= "15:31:00" :
-            historicParam = {"exchange": "NFO","symboltoken": symboltoken, "interval": "FIVE_MINUTE", "fromdate": f"{Candle_Date} 09:00", "todate": f"{Candle_Date} 15:30"}
+        if "09:20:00" <= get_live_datetime("live_time") <= "23:59:00":
+            historicParam = { "exchange": "NFO", "symboltoken": symboltoken, "interval": "FIVE_MINUTE", "fromdate": f"{last_trading_date} 09:00",  "todate": f"{last_trading_date} 15:30" }
             response = smartApi.getCandleData(historicParam)
-            if response.get("data"):
-                DATA = pd.DataFrame(response["data"], columns=['Time', 'Open', 'High', 'Low', 'Close', 'Volume'])
-                DATA['Time'] = pd.to_datetime(DATA['Time'])
-                DATA['Date'] = DATA['Time'].dt.strftime('%d-%m-%Y')
-                DATA['Time'] = DATA['Time'].dt.strftime('%H:%M')
-
-                # 9:15 और पिछले कैंडल के क्लोज प्राइस निकालें  Close_PC
-                close_price = DATA.loc[DATA['Time'] == First_Candle_Time, 'Close']
-                previous_price = DATA.loc[DATA['Time'] == Previous_Candal_Time, 'Close']
-
-                Tred = None  # Initialize Tred
-                if not close_price.empty and not previous_price.empty:
-                    Close_915 = float(close_price.values[0])  # Ensure numeric
-                    Close_PC = float(previous_price.values[0])  # Ensure numeric
-                    Tred = "Yes" if Close_915 > Close_PC else None
-                else:
-                    Close_915 = None
-                    Close_PC = None
-
-                # डेटा अपडेट करें
-                return { "Symbol": Symbol, "Symboltoken": symboltoken, "StrikePrice": StrikePrice , "OP_tipe" : OP_tipe  , "Close_915": Close_915, "Close_PC": Close_PC,"Tred" : Tred }
+            if not response.get("data"):
+                return None
+            columns = ['time', 'open', 'high', 'low', 'close', 'volume']
+            df = pd.DataFrame(response['data'], columns=columns)
+            df['time'] = pd.to_datetime(df['time'])
+            df['time_str'] = df['time'].dt.strftime('%H:%M')
+            Close_915 = df.loc[df['time_str'] == First_Candle_Time, 'close'].iloc[0] if not df[df['time_str'] == First_Candle_Time].empty else None
+            Close_PC = df.loc[df['time_str'] == Previous_Candle_Time, 'close'].iloc[0] if not df[df['time_str'] == Previous_Candle_Time].empty else None
+            Tred = "Yes" if (Close_915 and Close_PC and Close_915 > Close_PC) else None
+            return { "Symbol": Symbol, "Symboltoken": symboltoken, "StrikePrice": StrikePrice, "OP_tipe": OP_tipe, "Close_915": Close_915, "Close_PC": Close_PC, "Tred": Tred }
     except Exception as e:
-        print(f"fetch_Candal_Data function Error :", e)
+        print(f"fetch_Candle_Data_Fast function Error: {e}")
+        return None
 
-# # उदाहरण के लिए
-# fetch_Candal_Data(symboltoken,Symbol,StrikePrice,OP_tipe)
-# print(tabulate(pd.DataFrame(Candal_Data), headers='keys', tablefmt='pretty', showindex=True))
-#__________________________________________________________________________________________________________________________________________________________________________________________________________________
-
-# fetch_Token  # fetch_Token  # fetch_Token  # fetch_Token  # fetch_Token  # fetch_Token  # fetch_Token  # fetch_Token  # fetch_Token
-def fetch_Token(smartApi, Scrip_Data, strike, name="NIFTY", exch_seg="NFO"):
-    try :
+def fetch_Candle_Data(smartApi, Scrip_Data, strike, Next_Expiry, last_trading_date, First_Candle_Time):
+    try:
+        symbol_to_token = Scrip_Data.set_index('symbol')['token'].to_dict()
         CEstrike = strike
         PEstrike = str(int(strike) + 50)
-        CEToken = f"{name}{Next_Expiry}{CEstrike}CE"
-        PEToken = f"{name}{Next_Expiry}{PEstrike}PE"
-        CE_SymbolToken = Scrip_Data[Scrip_Data['symbol'].str.contains(CEToken, case=False, na=False)]['token'].iloc[0]
-        CE_Symbol = Scrip_Data[Scrip_Data['symbol'].str.contains(CEToken, case=False, na=False)]['symbol'].iloc[0]
-        PE_SymbolToken = Scrip_Data[Scrip_Data['symbol'].str.contains(PEToken, case=False, na=False)]['token'].iloc[0]
-        PE_Symbol = Scrip_Data[Scrip_Data['symbol'].str.contains(PEToken, case=False, na=False)]['symbol'].iloc[0]
-        Call = fetch_Candal_Data(CE_SymbolToken,CE_Symbol,CEstrike,"CE", smartApi)
-        Put  = fetch_Candal_Data(PE_SymbolToken,PE_Symbol,PEstrike,"PE", smartApi)
-        Candal_Data = { "CE" : Call , "PE" : Put }
-        return Candal_Data
+        CEToken = f"NIFTY{Next_Expiry}{CEstrike}CE"
+        PEToken = f"NIFTY{Next_Expiry}{PEstrike}PE"
+        CE_SymbolToken = symbol_to_token.get(CEToken)
+        PE_SymbolToken = symbol_to_token.get(PEToken)
+        if not CE_SymbolToken or not PE_SymbolToken:
+            raise ValueError("Symbol token not found for CE or PE")
+        Previous_Candle_Time = get_candle_times(time_frame=5, Candle_no=-2, format="%H:%M")
+        with ThreadPoolExecutor(max_workers=2) as executor:
+            ce_future = executor.submit( fetch_Candle_Data_Fast, CE_SymbolToken, CEToken, CEstrike, "CE",last_trading_date, First_Candle_Time, Previous_Candle_Time, smartApi )
+            pe_future = executor.submit( fetch_Candle_Data_Fast, PE_SymbolToken, PEToken, PEstrike, "PE", last_trading_date, First_Candle_Time, Previous_Candle_Time, smartApi)
+            CE_Candle_Data = ce_future.result()
+            PE_Candle_Data = pe_future.result()
+        return {"CE": CE_Candle_Data, "PE": PE_Candle_Data}
     except Exception as e:
-        print( "fetch_Token function Error : ", e )
+        print(f"fetch_Candle_Data function Error: {e}")
+        return None
 
-# # उदाहरण के लिए
-# Candal_Data = fetch_Token(smartApi, Scrip_Data, ATM_Strik)
-# print(Candal_Data)
-# fetch_Token("24500")
+# strike = 24250
+# All_Scrip_Data = fetch_All_Scrip_Data()
+# Scrip_Data = fetch_Scrip_Data(All_Scrip_Data)
+# Curnent_Expiry, Next_Expiry, Expiry_List = fetch_ExpiryDate (Scrip_Data)
+# last_trading_date    = get_last_trading_date(fmt="%Y-%m-%d")
+# First_Candle_Time    = get_candle_times(time_frame=5, Candle_no=1, format="%H:%M")
+
+# Candal_Data = fetch_Candle_Data(smartApi, Scrip_Data, strike, Next_Expiry, last_trading_date, First_Candle_Time)
 # print(tabulate(pd.DataFrame(Candal_Data), headers='keys', tablefmt='pretty', showindex=True))
-#________________________________________________________________________________________________________________________________________________________
+#______________________________________________________________________________________________________________________________________________________________________________
+
 
 
 
