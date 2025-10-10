272196843a718cb4ee36aad05abafa9d69785d6d
Kaklotar9985 <kaklotar.mitul@gmail.com>
2025-10-10 16:21:27 +0530

    Update ICICIHistorical_V3.py

--- ICICIHistorical_V3.py
+++ ICICIHistorical_V3.py
@@ -2,6 +2,29 @@ from IPython.display import clear_output
 import logging
 # Disable Breeze API debug logs
 logging.getLogger("APILogger").setLevel(logging.CRITICAL)
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#  ICICI_Login   ICICI_Login     ICICI_Login  ICICI_Login     ICICI_Login     ICICI_Login     ICICI_Login     ICICI_Login      ICICI_Login   ICICI_Login
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+from breeze_connect import BreezeConnect
+import urllib
+def ICICI_Login(session_token, APIKEY, SecretKey):
+    try:
+        if not session_token:
+            login_url = f"https://api.icicidirect.com/apiuser/login?api_key={urllib.parse.quote_plus(APIKEY)}"
+            print("Please login using this link to generate session_token:")
+            print(login_url)
+            return None
+        breeze = BreezeConnect(api_key=APIKEY)
+        breeze.generate_session(api_secret=SecretKey, session_token=session_token)
+        print("Login Successful ✅")
+        return breeze
+    except Exception as e:
+        print("Login Failed ❌")
+        login_url = f"https://api.icicidirect.com/apiuser/login?api_key={urllib.parse.quote_plus(APIKEY)}"
+        print("Please login using this link to generate session_token:\n",)
+        print(login_url,"\n")
+        return None
+#=======================================================================================================================================================================
 
 #----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 #  Data_Error  Error_Data_to_Excel     Data_Error  Error_Data_to_Excel     Data_Error  Error_Data_to_Excel     Data_Error  Error_Data_to_Excel      Data_Error  Error_Data_to_Excel
